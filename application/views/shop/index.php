622496ad2987fc5391de079fa190618fd59410ae
howtomakeaturn <howtomakeaturn@hotmail.com>
2014-07-27 10:57:20 +0800

    add tabs

--- application/views/shop/index.php
+++ application/views/shop/index.php
@@ -1,5 +1,33 @@
+<ul class="nav nav-tabs layout-tabs" role="tablist">
+  <li><a href="#">開始設計</a></li>
+  <li><a href="#">設計師款</a></li>
+  <li><a href="#">如何設計</a></li>
+</ul>
+<div id="main-carousel" class="carousel slide carousel-margin" data-ride="carousel">
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+
+        <?php foreach($items as $index => $item): ?>
+        <div class="item <?php if($index===1) echo 'active' ?>">
+            <img src="/upload/<?php echo $item->image ?>">
+            <div class="carousel-caption">
+                <?php echo $item->name ?>
+            </div>            
+            
+        </div>    
+        <?php endforeach; ?>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#main-carousel" data-slide="prev">
+      <span class="icon-prev"></span>
+    </a>
+    <a class="right carousel-control" href="#main-carousel" data-slide="next">
+      <span class="icon-next"></span>
+    </a>
+  </div>
 
 <h3 class='text-center'>精選推薦商品</h3>
+
 
 
 <div class="row">
@@ -24,6 +52,14 @@
     .item-image-box{
         height: 300px;
         overflow: hidden;
+    }
+    
+    .carousel-inner img{
+        margin: 0 auto;
+    }
+    
+    .layout-tabs{
+        margin-bottom: 20px;
     }
 
 </style>
